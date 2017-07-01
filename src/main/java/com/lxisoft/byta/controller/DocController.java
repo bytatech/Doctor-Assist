4722e3bad0908dc3774f1a67933e9fe20ec04af0
Basil <basil.jude@lxisoft.com>
2017-06-30 20:40:48 -0700

    controller class added

---
+++ src/main/java/com/lxisoft/byta/controller/DocController.java
@@ -0,0 +1,18 @@
+
+package com.lxisoft.byta.controller ;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This is a rest controller class which is used to address request and response
+ * 
+ * @author basil1
+ * @version 1.0.0
+ */
+
+@RestController
+
+public class DocController {
+
+}
